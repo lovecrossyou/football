ca8f836a55e26fdd7ac74e13dfb3135af1c9ca34
猪猪侠 <345841634@qq.com>
2017-10-31 16:09:30 +0800

    xx

--- spider.js
+++ spider.js
@@ -3,7 +3,6 @@ let request = require('request');
 let iconv = require('iconv-lite');
 
 let url = 'http://www.okooo.com/livecenter/football/?date=2017-10-30';
-
 startRequest = (url) => {
     request.get({
         url: url,
@@ -16,58 +15,47 @@ startRequest = (url) => {
             }
         });
         let trList = $('table').children('tr');
-        trList = trList.slice(0,3);
-        for (let i=0;i<trList.length;i++) {
-            let tdArr = trList.eq(i+2).find("td");
+        trList = trList.slice(0, 10);
+        for (let i = 0; i < trList.length; i++) {
+            let tdArr = trList.eq(i + 2).find("td");
             let game_type = tdArr.eq(1).text();//赛事类型
+            if(game_type.length==0)continue;
             let game_time = tdArr.eq(2).text();//开始时间
             let game_status = tdArr.eq(3).text();//比赛状态
             let game_host = tdArr.eq(4).find('a').text();//主队
             let game_result = tdArr.eq(5).text();//比赛结果
-            let game_guest = tdArr.eq(6).find('a').text();//客队
-            let game_guest1 = tdArr.eq(7).text();//结果
-            let game_guest2 = tdArr.eq(8).text();//结果
-            let game_guest3 = tdArr.eq(9).text();//结果
-            let game_guest4 = tdArr.eq(10).text();//结果
-
-
+            let game_guest = tdArr.eq(6);//客队
+            let guestNode = $(game_guest).children('a');
+            let guest = guestNode.eq(0).text();
+            let game_guest3 = tdArr.eq(9);//胜 平 负
+            let valuesNode = $(game_guest3).children('span');
+            let values = [];
+            values.push({
+                win: valuesNode.eq(0).text(),
+                even: valuesNode.eq(1).text(),
+                lost: valuesNode.eq(2).text(),
+            });
             let game_guest5 = tdArr.eq(11);
             let hrefs = $(game_guest5).children('a');
-            let urls = [] ;
-            for (let i=0;i<hrefs.length;i++) {
-                let tdArr = hrefs.eq(i).find("a");
-                urls.push({
-                    url1:tdArr.eq(0).text(),
-                    url2:tdArr.eq(1).text(),
-                    url3:tdArr.eq(2).text()
-                });
+            let urls = [];
+            const prefixUrl = 'http://www.okooo.com';
+            urls.push({
+                oddUrl: prefixUrl + hrefs.eq(0).attr('href'),
+                historyUrl: prefixUrl + hrefs.eq(1).attr('href'),
+            });
+
+            let game = {
+                game_type: game_type,
+                game_time: game_time,
+                game_status: game_status,
+                game_host: game_host,
+                game_guest: guest,
+                game_result: game_result,
+                game_guest3: values,
+                game_guest5: urls,
             }
-
-            console.log({
-                game_type:game_type,
-                game_time:game_time,
-                game_status:game_status,
-                game_host:game_host,
-                game_guest:game_guest,
-                game_result:game_result,
-                game_guest1:game_guest1,
-                game_guest2:game_guest2,
-                game_guest3:game_guest3,
-                game_guest4:game_guest4,
-                game_guest5:urls,
-            })
+            console.log(game);
         }
-
-
-
-        // $('table tr').each(function (i, elem) {
-        //     $(this).find('td').map(function (index, e) {
-        //         let a = $(this).text().trim();
-        //         console.log(a);
-        //         console.log('td*******************************************',index);
-        //     });
-        //     console.log('tr*******************************************',i);
-        // });
     });
 }
 
